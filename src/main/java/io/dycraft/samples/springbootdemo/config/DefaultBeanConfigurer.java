a7143bd0e214dff643b50e4bbf8c5e086dfbfa24
dycraft <dy_private@163.com>
2019-06-17 10:35:06 +0800

    Use lambda to load UserDetailService bean

--- src/main/java/io/dycraft/samples/springbootdemo/config/DefaultBeanConfigurer.java
+++ src/main/java/io/dycraft/samples/springbootdemo/config/DefaultBeanConfigurer.java
@@ -1,10 +1,12 @@
 package io.dycraft.samples.springbootdemo.config;
 
-import io.dycraft.samples.springbootdemo.security.UserDetailsServiceImpl;
+import io.dycraft.samples.springbootdemo.model.User;
 import io.dycraft.samples.springbootdemo.service.UserService;
+import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -21,6 +23,18 @@ public class DefaultBeanConfigurer {
 
     @Bean
     public UserDetailsService userDetailsService(UserService userService) {
-        return new UserDetailsServiceImpl(userService);
+        return username -> {
+            User user = userService.getByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' not found"));
+            return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(new ArrayList<>())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+        };
     }
 }
